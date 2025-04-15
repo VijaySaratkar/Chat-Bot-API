9e256fb8bc0ced41e2b06580fbd991ad800a96c1
Vijay Saratkar <vijaydeveloper2002@gmail.com>
2025-04-15 17:14:30 +0530

    fix: minor change

--- src/models/User.js
+++ src/models/User.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: mongoose.Schema.Types.Mixed, required: true }, 
 });
 
 export default mongoose.model('User', userSchema);
